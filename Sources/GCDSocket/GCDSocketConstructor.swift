8110a5f58973265f7708937719808c0d0ddf624f
Steve Trewick <steve.trewick@icloud.com>
2025-01-24 20:50:16 +0000

    add comments

--- Sources/GCDSocket/GCDSocketConstructor.swift
+++ Sources/GCDSocket/GCDSocketConstructor.swift
@@ -1,7 +1,25 @@
 import Foundation
 
+
+/*
+ 
+ here we can construct, to our black heart's content the various bits and bobs of the
+ GCDSocket fam, they should be fairly self explanatory, but still, I have to write docs
+ one day, so ...
+ 
+ one thing to note is that as of the moment this library only supports unix domain and local tcp/ip
+ sockets and I have no current plans to change that, if you need something else, you should be using
+ a better library.  THis is just for hacking around on your local system and mugging things like
+ usbmuxd, which is what I wrote for originally, TBH
+ 
+*/
+
+
 public struct GCDSocketConstructor {
   
+  /*
+    SOCK_STREAM and SOCK_DGRAM for ip sockets
+  */
   public enum SockType {
     case stream, datagram
   }
@@ -10,26 +28,40 @@ public struct GCDSocketConstructor {
   public init() {}
   
   
-  public func domainSocket(path: String) -> GCDSocketDescriptor<sockaddr_un> {
+  /*
+    construct a domain socket descriptor from a path
+  */
+  public func domainSocket ( path: String ) -> GCDSocketDescriptor<sockaddr_un> {
     GCDSocketDescriptor (
       handle  : socket(AF_UNIX, SOCK_STREAM, 0),
       sockAddr: domainaddr ( path: path )
     )
   }
   
-  
+  /*
+    construct a domain socket client from a path, the socket is not connected or running,
+    it remains quiescent, awaiting your no doubt malicious attention
+  */
   public func domainSocketClient ( path: String ) -> GCDSocketClient<sockaddr_un> {
     GCDSocketClient (
       descriptor: domainSocket(path: path)
     )
   }
   
+  /*
+    construct a whole heckin unix domain socket server from just. a. path.
+    bargain of the century, I tell you
+  */
   public func domainSocketServer ( path: String ) -> GCDSocketServer<sockaddr_un> {
     GCDSocketServer (
       descriptor: domainSocket(path: path)
     )
   }
   
+  
+  /*
+    construct a local TCP/UDP socket descriptor
+  */
   public func localSocket ( port: UInt16, type: SockType ) -> GCDSocketDescriptor<sockaddr_in> {
     
     func sock(of type: SockType) -> Int32 {
@@ -46,19 +78,30 @@ public struct GCDSocketConstructor {
     
   }
   
+  /*
+    construct an actual GCDSocketClient given just the local port and type,
+    as with the domain client, it will laze around doing nothing you .connect() it
+  */
   public func loacalSocketClient ( port: UInt16, type: SockType ) -> GCDSocketClient<sockaddr_in> {
     GCDSocketClient (
       descriptor: localSocket(port: port, type: type)
     )
   }
   
-  
+  /*
+    construct a living, breathing TCP/UDP server on a local port for your own twisted amusement
+    don't forget to .resume() it though or it will just lie around grumbling
+  */
   public func localSocketServer ( port: UInt16, type: SockType ) -> GCDSocketServer<sockaddr_in> {
     GCDSocketServer (
       descriptor: localSocket (port: port, type: type)
     )
   }
   
+  
+  /*
+    utility funcs for building the sockaddrs
+  */
   public func localaddr ( fam: Int32, port: in_port_t ) -> sockaddr_in {
     
     var sockaddr = sockaddr_in()
--- Sources/GCDSocket/GCDSocketPointerMangler.swift
+++ Sources/GCDSocket/GCDSocketPointerMangler.swift
@@ -1,13 +1,31 @@
 import Foundation
 
 
+
+/*
+  today in "I am not typing that fugly monstrosity like 4 times"
+  we have to a fucky pointer dance around a lot of the Darwin POSIX calls because we are
+  not in a casually typed language like C. Anyway, here we go through the dance of casting
+  through from a sockaddr_* to a sockaddr, so we can just do like
+ 
+  let bres = rebound(from: descriptor.sockAddr) { saddr, slen in
+    Darwin.bind(sockFD, saddr, slen)
+  }
+  
+  which is only marginally less ugly but easier to grok
+ 
+  oh, except for accept, because it wants a pointer for length, anyway
+ 
+  I added this as a protocol with a default just becuase I was kinda bored TBH.
+*/
+
 protocol GCDSocketPointerMangler {
-  func rebound <T:GCDSocketAddress> ( from addr: T, _ exec: (UnsafeMutablePointer<sockaddr>, socklen_t) -> Int32 ) -> Int32
+  func rebound <T: GCDSocketAddress> ( from addr: T, _ exec: (UnsafeMutablePointer<sockaddr>, socklen_t) -> Int32 ) -> Int32
 }
 
 extension GCDSocketPointerMangler {
   
-  func rebound <T:GCDSocketAddress> ( from addr: T, _ exec: (UnsafeMutablePointer<sockaddr>, socklen_t) -> Int32 ) -> Int32 {
+  func rebound <T: GCDSocketAddress> ( from addr: T, _ exec: (UnsafeMutablePointer<sockaddr>, socklen_t) -> Int32 ) -> Int32 {
     
     var addr = addr
   
