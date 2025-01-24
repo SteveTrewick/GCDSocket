ba705a97699404dda9a78be5cae74fdf49936552
Steve Trewick <steve.trewick@icloud.com>
2025-01-24 19:14:56 +0000

    completely rewritten now

---
+++ Sources/GCDSocket/GCDSocketAPI.swift
@@ -0,0 +1,87 @@
+import Foundation
+
+public struct GCDSocketAPI {
+  
+  public enum SockType {
+    case stream, datagram
+  }
+  
+  
+  public init() {}
+  
+  
+  public func domainSocket(path: String) -> GCDSocketDescriptor<sockaddr_un> {
+    GCDSocketDescriptor (
+      handle  : socket(AF_UNIX, SOCK_STREAM, 0),
+      sockAddr: domainaddr ( path: path )
+    )
+  }
+  
+  
+  public func domainSocketClient ( path: String ) -> GCDSocketClient<sockaddr_un> {
+    GCDSocketClient (
+      descriptor: domainSocket(path: path)
+    )
+  }
+  
+  public func domainSocketServer ( path: String ) -> GCDSocketServer<sockaddr_un> {
+    GCDSocketServer (
+      descriptor: domainSocket(path: path)
+    )
+  }
+  
+  public func localSocket ( port: UInt16, type: SockType ) -> GCDSocketDescriptor<sockaddr_in> {
+    
+    func sock(of type: SockType) -> Int32 {
+      switch type {
+        case .stream  : return socket(AF_INET, SOCK_STREAM, 0)
+        case .datagram: return socket(AF_INET, SOCK_DGRAM,  0)
+      }
+    }
+    
+    return GCDSocketDescriptor (
+      handle  : sock(of: type),
+      sockAddr: localaddr(fam: AF_INET, port: port)
+    )
+    
+  }
+  
+  public func loacalSocketClient ( port: UInt16, type: SockType ) -> GCDSocketClient<sockaddr_in> {
+    GCDSocketClient (
+      descriptor: localSocket(port: port, type: type)
+    )
+  }
+  
+  
+  public func localSocketServer ( port: UInt16, type: SockType ) -> GCDSocketServer<sockaddr_in> {
+    GCDSocketServer (
+      descriptor: localSocket (port: port, type: type)
+    )
+  }
+  
+  public func localaddr ( fam: Int32, port: in_port_t ) -> sockaddr_in {
+    
+    var sockaddr = sockaddr_in()
+
+    sockaddr.sin_family      = sa_family_t(fam)
+    sockaddr.sin_port        = port.bigEndian
+    sockaddr.sin_addr.s_addr = INADDR_LOOPBACK.bigEndian
+    
+    return sockaddr
+  }
+
+  
+  
+  func domainaddr ( path: String ) -> sockaddr_un {
+    
+    var address = sockaddr_un()
+    
+    path.withCString { ptr in
+      withUnsafeMutablePointer(to: &address.sun_path.0) { dest in
+          _ = strcpy(dest, ptr)
+      }
+    }
+    return address
+  }
+
+}
---
+++ Sources/GCDSocket/GCDSocketServer.swift
@@ -0,0 +1,82 @@
+import Foundation
+
+
+
+
+public class GCDSocketServer<T: GCDSocketAddress> : GCDSocket, GCDSocketPointerMangler {
+  
+  
+  public let descriptor : GCDSocketDescriptor<T>
+  public let backlog    : Int32
+  public var accept     : ((Result<GCDSocket, GCDSocketError>) -> Void)? = nil
+  
+  private var accepting : Bool = false
+  
+  
+  public init ( descriptor: GCDSocketDescriptor<T>, backlog: Int32 = 8 ) {
+    
+    self.descriptor = descriptor
+    self.backlog    = backlog
+    
+    super.init(fd: descriptor.handle)
+  }
+  
+  
+  
+  public override func resume() {
+    
+    accepting = true
+    
+    sockQ.async { [self] in
+      
+      let bres = rebound(from: descriptor.sockAddr) { saddr, slen in
+        Darwin.bind(sockFD, saddr, slen)
+      }
+      
+      if bres != 0 {
+        accept? ( .failure( .bind(errno)) )
+        accepting = false
+        return
+      }
+      
+      let lres = Darwin.listen(sockFD, backlog)
+      if  lres != 0 {
+        accept? ( .failure( .listen(errno)) )
+        accepting = false
+        return
+      }
+      
+      
+      
+      while accepting {
+        
+        let clisockaddr = T()
+        var cliaddrlen  = socklen_t(MemoryLayout<T>.size)
+        
+        let clisock = rebound(from: clisockaddr) { saddr, slen in
+          Darwin.accept(sockFD, saddr, &cliaddrlen)
+        }
+        
+        if clisock == -1 {
+          accept? (.failure( .accept(errno)) )
+          accepting = false
+          break
+        }
+        
+        accept?( .success(GCDSocket(fd: clisock) ) )
+        
+      }
+    }
+  }
+  
+  
+  public func suspend() {
+    accepting = false
+  }
+  
+  public override func close() {
+    accepting = false
+    Darwin.close(sockFD)
+  }
+  
+}
